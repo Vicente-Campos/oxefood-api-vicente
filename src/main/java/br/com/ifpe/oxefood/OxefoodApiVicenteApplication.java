556ba65e57c9b02f535e36c175859ec433fea467
Marleide Alves <aluno@ifpe>
2024-02-21 18:19:08 -0300

    primeiros arquivos do projeto

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiVicenteApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiVicenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiVicenteApplication.class, args);
+	}
+
+}
