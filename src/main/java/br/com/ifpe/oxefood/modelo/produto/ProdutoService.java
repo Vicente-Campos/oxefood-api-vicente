620a1405a1ed2d6377d9e10b80eca6407958803a
aluno@ifpe <aluno@ifpe>
2024-03-21 15:21:33 -0300

    implementando a listagem back-end

--- src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/produto/ProdutoService.java
@@ -1,9 +1,12 @@
 package br.com.ifpe.oxefood.modelo.produto;
 
 import java.time.LocalDate;
-import jakarta.transaction.Transactional;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class ProdutoService {
@@ -18,6 +21,16 @@ public class ProdutoService {
        produto.setVersao(1L);
        produto.setDataCriacao(LocalDate.now());
        return repository.save(produto);
+   }
+
+   public List<Produto> listarTodos(){
+
+        return repository.findAll();
+   }
+
+   public Produto obterPorID(Long id){
+
+        return repository.findById(id).get();
    }
 
 }
