9fd2393cb1327ca76b339cbcd0552785a554b67e
aluno@ifpe <aluno@ifpe>
2024-07-04 17:51:51 -0300

    simulado

---
+++ src/main/java/br/com/ifpe/oxefood/modelo/fornecedor/Fornecedor.java
@@ -0,0 +1,46 @@
+package br.com.ifpe.oxefood.modelo.fornecedor;
+
+import java.time.LocalDate;
+
+import org.hibernate.annotations.SQLRestriction;
+import br.com.ifpe.oxefood.util.entity.EntidadeAuditavel;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Fornecedor")
+@SQLRestriction("habilitado = true")
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Fornecedor extends EntidadeAuditavel {
+
+    @Column
+    private String nome;
+
+    @Column
+    private String endereco;
+
+    @Column
+    private LocalDate dataFundacao;
+
+    @Column
+    private double valorMercado;
+
+    @Column
+    private String paginaWeb;
+
+    @Column
+    private String contatoVendedor;
+    
+}
