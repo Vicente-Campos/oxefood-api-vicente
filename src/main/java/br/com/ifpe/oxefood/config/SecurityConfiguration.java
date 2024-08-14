33f32f2d0b9a1a91dfe72e009be9056c1ff38718
aluno <>
2024-08-14 17:34:20 -0300

    Atualização do SecurityConfiguration

--- src/main/java/br/com/ifpe/oxefood/config/SecurityConfiguration.java
+++ src/main/java/br/com/ifpe/oxefood/config/SecurityConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -33,22 +34,25 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.csrf()
-                .disable()
-                .authorizeHttpRequests()               
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(authorize -> authorize
+
                 .requestMatchers(HttpMethod.POST, "/api/cliente").permitAll()
-                 .requestMatchers(HttpMethod.POST, "/api/auth").permitAll()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement()
+                .requestMatchers(HttpMethod.POST, "/api/auth").permitAll()
+
+                .anyRequest().authenticated()
+
+            )
+            .sessionManagement((session) -> session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+            )            
+            .authenticationProvider(authenticationProvider)
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
+
 
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
